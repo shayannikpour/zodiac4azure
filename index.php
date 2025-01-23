a3f5dd9bcd0d98ea17bbd43b0c5d3222e7dcb752
Shayan Nikpour <shayannikpour2@gmail.com>
2025-01-23 10:59:56 -0800

    Name in green

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php $title = 'Home'; ?>
 
 <div class="container">
-    <h3>Shayan Nikpour</h3>
+    <h3 class="text-success">Shayan Nikpour</h3>
     <div class="mt-4 p-2 bg-secondary text-white rounded" style="width: 60%;">
         <h3>Check your zodiac.</h3>
         <p>
